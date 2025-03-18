dce6198ef6aa40adec41d07012f68277dbc07235
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-03-18 17:19:39 +0100

    feat: Un raccourci pour la directive v-on

--- main.js
+++ main.js
@@ -8,9 +8,6 @@ const description = 'les chaussettes sont belles!'
 const imageB = './assets/images/socks_blue.jpg'
 
 // déclaration du chemin absolu de l'image chaussette verte
-const imageG = './assets/images/socks_green.jpg'
-
-// déclaration du chemin absolu de l'image chaussette verte
 const etml = 'https://www.etml.ch/'
 
 // 
@@ -23,7 +20,6 @@ const app = Vue.createApp({
             product: product,
             description: description,
             imageB: imageB,
-            imageG: imageG,
             url: etml,
             inventory: 6,
             onSale: true,
