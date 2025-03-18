a8609f303865186068b1f28bda5c64de6e680497
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-03-18 15:18:05 +0100

    feat: Ajout d'une image à nos données

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ const app = Vue.createApp({
     data() {
         return{
             product: product,
-            description: description
+            description: description,
+            image: './assets/images/socks_blue.jpg'
         }
     }
 });
