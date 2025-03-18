bb38c3ce8eb5c1b1b169538b0d8a6dcf37406173
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-03-18 14:25:29 +0100

    feat: Création d'une application Vue

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 // une chaussette qui est un produit
 const product = 'Socks'
+
+// création d'application Vue
+const app = Vue.createApp({
+    data() {
+        return{
+            product: product
+        }
+    }
+});
