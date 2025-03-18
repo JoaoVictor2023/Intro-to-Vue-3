0977b4630136d68215d3521aa1c3f6204e287dfa
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-03-18 16:08:50 +0100

    feat: La directive "v-if"

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ const product = 'Socks'
 // déclaration de description
 const description = 'les chaussettes sont belles!'
 
+// déclaration du chemin absolu de l'image chaussette bleue
+const imageB = './assets/images/socks_blue.jpg'
+
+// déclaration du chemin absolu de l'image chaussette verte
+const imageG = './assets/images/socks_green.jpg'
+
+// déclaration du chemin absolu de l'image chaussette verte
+const etml = 'https://www.etml.ch/'
+
 // création d'application Vue
 const app = Vue.createApp({
     data() {
         return{
             product: product,
             description: description,
-            image: './assets/images/socks_green.jpg'
+            imageB: imageB,
+            imageG: imageG,
+            url: etml,
+            inStock: true
         }
     }
 });
