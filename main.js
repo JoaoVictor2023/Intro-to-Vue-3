b7090c3058800697ddc7af32b6b6685af212bbbd
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-03-25 13:37:16 +0100

    fix: a vous de coder OnSale

--- main.js
+++ main.js
@@ -37,7 +37,6 @@ const app = Vue.createApp({
             sizes: ['S', 'M', 'L', 'XL'],
             cart: cart,
             selectedVariant: 0,
-            onSale: true
         }
     },
     methods: {
@@ -64,9 +63,11 @@ const app = Vue.createApp({
         inStock(){
             return this.variants[this.selectedVariant].quantity
         },
-        onSale(){
-                return this.title + ' ' + 'is on sale'
-            
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
         }
     }
 });
