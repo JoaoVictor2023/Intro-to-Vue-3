ee4c17917d0373e78cc935babc79abbde451149e
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-03-25 13:53:11 +0100

    Création de notre premier composant(Données et méthodes)

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -27,4 +27,53 @@ app.component('product-display', {
                 </div>
             </div>
         </div>`,
+    data() {
+        return {
+            product: product,
+            brand: 'Vue Mastery',
+            description: description,
+            url: etml,
+            inventory: inventory,
+            onSale: true,
+            details: ['50% cooton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: greenSocks, quantity: 50 },
+                { id: 2235, color: 'blue', image: blueSocks, quantity: 0 }
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
+            cart: cart,
+            selectedVariant: 0,
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.imageB = variantImage
+        },
+        removeCart() {
+            this.cart -= 1
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
+        }
+    }
 })
